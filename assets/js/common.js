fee0425f94990753225404d56ecdc83fae1f0066
kimkyungm13 <kyung_m13@naver.com>
2024-03-10 21:16:25 +0900

    22

--- assets/js/common.js
+++ assets/js/common.js
@@ -2,9 +2,8 @@ $(function () {
     // 스크롤 헤더
     window.addEventListener('scroll', function () {
         var scrollPosition = window.scrollY;
-
-        if (scrollPosition > 0) { // 스크롤 위치가 최상단이 아닌 경우
-            // 마우스 휠을 아래로 스크롤할 때와 동일한 동작 수행
+        winH = window.innerHeight;
+        if (scrollPosition > (winH / 3)) {
             $('#header').addClass('wheel');
             $('.side_bnr_wrap').css('display', 'block');
             // 추가로 필요한 동작을 여기에 추가합니다.
@@ -22,11 +21,46 @@ $(function () {
         if (curr > lastScroll
 
         ) {
-            $('.quick').css('display', 'none')
+            $('.quick').css('display', 'none');
         } else {
             $('.quick').css('display', 'block')
         }
         lastScroll = curr
     });
+    function reveal() {
+        var reveals = document.querySelectorAll(".text");
+
+        for (var i = 0; i < reveals.length; i++) {
+            var windowHeight = window.innerHeight;
+            var elementTop = reveals[i].getBoundingClientRect().top;
+            var elementVisible = 150;
+
+            if (elementTop < windowHeight - elementVisible) {
+                reveals[i].classList.add("active");
+            } else {
+                reveals[i].classList.remove("active");
+            }
+        }
+    }
+
+    window.addEventListener("scroll", reveal);
+
 })
 
+$(window).scroll(function () {
+    var sectionChallenge = $('.sc-challenge').offset().top;
+    var sectionProve = $('.sc-prove').offset().top;
+    var sectionPossibility = $('.sc-possibility').offset().top;
+
+    if ($(window).scrollTop() >= sectionChallenge && $(window).scrollTop() < sectionPossibility) {
+        $('.logo').addClass('black'); // 섹션 one에 도달하면 one 클래스를 추가
+    } else {
+        $('.logo').removeClass('black'); // 섹션 one보다 위에 있으면 one 클래스를 제거
+    } if ($(window).scrollTop() >= sectionPossibility) {
+        $('.logo').removeClass('black'); // 섹션 three 이후에는 one 클래스를 제거
+        $('body').addClass('black')
+    } else {
+        $('body').removeClass('black')
+
+    }
+});
