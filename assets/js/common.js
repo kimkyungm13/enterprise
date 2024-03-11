f9038c2c65368bea97ca88bcccb15b0f93dae494
kimkyungm13 <kyung_m13@naver.com>
2024-03-12 00:08:55 +0900

    gsap

--- assets/js/common.js
+++ assets/js/common.js
@@ -2,19 +2,14 @@ $(function () {
     $('.lang').click(function () {
         $(this).toggleClass('active')
     })
-    // 스크롤 헤더
     window.addEventListener('scroll', function () {
         var scrollPosition = window.scrollY;
         winH = window.innerHeight;
         if (scrollPosition > (winH / 3)) {
             $('#header').addClass('wheel');
-            $('.side_bnr_wrap').css('display', 'block');
-            // 추가로 필요한 동작을 여기에 추가합니다.
         } else {
-            // 스크롤이 최상단에 도달했을 때 실행할 코드
             $('header').removeClass('wheel');
             $('.menu-wrap').css('display', 'none');
-            // 추가로 필요한 동작을 여기에 추가합니다.
         }
     });
     //퀵메뉴
@@ -30,43 +25,85 @@ $(function () {
         }
         lastScroll = curr
     });
-    function reveal() {
-        var reveals = document.querySelectorAll(".text");
 
-        for (var i = 0; i < reveals.length; i++) {
-            var windowHeight = window.innerHeight;
-            var elementTop = reveals[i].getBoundingClientRect().top;
-            var elementVisible = 150;
+    $(window).scroll(function () {
+        var sectionChallenge = $('.sc-challenge').offset().top;
+        var sectionProve = $('.sc-prove').offset().top;
+        var sectionPossibility = $('.sc-possibility').offset().top;
 
-            if (elementTop < windowHeight - elementVisible) {
-                reveals[i].classList.add("active");
-            } else {
-                reveals[i].classList.remove("active");
-            }
+        if ($(window).scrollTop() >= sectionChallenge && $(window).scrollTop() < sectionPossibility) {
+            $('#header').addClass('black');
+        } else {
+            $('#header').removeClass('black');
+        } if ($(window).scrollTop() >= sectionPossibility) {
+            $('#header').removeClass('black');
+            $('body').addClass('black')
+        } else {
+            $('body').removeClass('black')
+
         }
-    }
+    });
+    // window.addEventListener("scroll", reveal);
+    const intro = gsap.timeline();
+    intro.from(".description .t1", { opacity: 0, duration: 1 })
+        .to(".description .t1", { opacity: 1, duration: 1 })
+        .to(".description .t1", { opacity: 0, duration: 1 })
+        .from(".description .t2", { opacity: 0, duration: 1 }, "<") // 이전 페이드 아웃이 끝난 후 실행
+        .to(".description .t2", { opacity: 1, duration: 1 })
+        .to(".description .t2", { opacity: 0, duration: 1 })
+        .from(".description .t3", { opacity: 0, duration: 1 }, "<")
+        .to(".description .t3", { opacity: 1, duration: 1 })
+        .to(".description .t3", { opacity: 0, duration: 1 })
+        .from(".description .t4", { opacity: 0, duration: 1 }, "<")
+        .to(".description .t4", { opacity: 1, duration: 1 })
+        .to(".description .t4", { opacity: 0, duration: 1 });
 
-    window.addEventListener("scroll", reveal);
+    ScrollTrigger.create({
+        animation: intro,
+        trigger: ".sc-intro",
+        start: "top top",
+        end: "+=6000",
+        scrub: true,
+        pin: true,
+        anticipatePin: 1,
+        markers: true
+    });
 
+
+
+    const showcase = gsap.timeline();
+    showcase.to(".sc-showcase .case01 .txt", { opacity: 1, duration: 2 })
+        .to(".sc-showcase .line01", { xPercent: 100 }, "text")
+        .to(".sc-showcase .line03", { xPercent: -100 }, "text")
+        .to(".sc-showcase .case01 .txt", { opacity: 0, duration: 2 })
+
+    ScrollTrigger.create({
+        animation: showcase,
+        trigger: ".sc-showcase",
+        start: "top top",
+        end: "+=3000",
+        scrub: true,
+        pin: true,
+        anticipatePin: 1,
+        markers: true
+    });
+
+    // const introTl = gsap.timeline();
+    // introTl.from(".sc-intro .description .text.t1", { opacity: 0, stagger: 0.2, y: -100, borderRadius: 200 })
+    //     .from(".sc-intro .description .text.t2", { opacity: 0, stagger: 0.2, y: -100, borderRadius: 200 })
+
+    // ScrollTrigger.create({
+    //     trigger: ".sc-intro",
+    //     start: "top top",
+    //     end: "+=600vh",
+    //     scrub: true,
+    //     pin: true,
+    //     anticipatePin: 1,
+    //     markers: true
+    // });
 })
 
-$(window).scroll(function () {
-    var sectionChallenge = $('.sc-challenge').offset().top;
-    var sectionProve = $('.sc-prove').offset().top;
-    var sectionPossibility = $('.sc-possibility').offset().top;
 
-    if ($(window).scrollTop() >= sectionChallenge && $(window).scrollTop() < sectionPossibility) {
-        $('#header').addClass('black');
-    } else {
-        $('#header').removeClass('black');
-    } if ($(window).scrollTop() >= sectionPossibility) {
-        $('#header').removeClass('black');
-        $('body').addClass('black')
-    } else {
-        $('body').removeClass('black')
-
-    }
-});
 
 
 // $(window).on('scroll', function () {
@@ -83,3 +120,4 @@ $(window).scroll(function () {
 
 //     $('#header').toggleClass('black', $('#heading').toggleClass('black', scrollTop >= sectionChallenge && scrollTop < sectionProve || scrollTop >= sectionProve && scrollTop < sectionGallery || scrollTop >= sectionGallery))
 // });
+
