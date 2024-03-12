70f132cd1396c1ac77cc3d682923d0a40219acc6
kimkyungm13 <kyung_m13@naver.com>
2024-03-12 13:11:07 +0900

    1

--- assets/js/common.js
+++ assets/js/common.js
@@ -3,7 +3,7 @@ $(function () {
         $(this).toggleClass('active')
     })
     window.addEventListener('scroll', function () {
-        var scrollPosition = window.scrollY;
+        const scrollPosition = window.scrollY;
         winH = window.innerHeight;
         if (scrollPosition > (winH / 3)) {
             $('#header').addClass('wheel');
@@ -12,24 +12,28 @@ $(function () {
             $('.menu-wrap').css('display', 'none');
         }
     });
-    //퀵메뉴
+
     let lastScroll = 0;
     $(window).scroll(function () {
         curr = $(this).scrollTop();
-        if (curr > lastScroll
+        if (curr < lastScroll
 
         ) {
-            $('.quick').css('display', 'none');
+            $('.top').addClass('show');
         } else {
-            $('.quick').css('display', 'block')
+            $('.top').removeClass('show')
         }
         lastScroll = curr
     });
+    $('.top').on('click', function (e) {
+        e.preventDefault();
+        $('html, body').animate({ scrollTop: 0 }, 'slow');
+    });
 
     $(window).scroll(function () {
-        var sectionChallenge = $('.sc-challenge').offset().top;
-        var sectionProve = $('.sc-prove').offset().top;
-        var sectionPossibility = $('.sc-possibility').offset().top;
+        const sectionChallenge = $('.sc-challenge').offset().top;
+        const sectionProve = $('.sc-prove').offset().top;
+        const sectionPossibility = $('.sc-possibility').offset().top;
 
         if ($(window).scrollTop() >= sectionChallenge && $(window).scrollTop() < sectionPossibility) {
             $('#header').addClass('black');
@@ -48,7 +52,7 @@ $(function () {
     intro.from(".description .t1", { opacity: 0, duration: 1 })
         .to(".description .t1", { opacity: 1, duration: 1 })
         .to(".description .t1", { opacity: 0, duration: 1 })
-        .from(".description .t2", { opacity: 0, duration: 1 }, "<") // 이전 페이드 아웃이 끝난 후 실행
+        .from(".description .t2", { opacity: 0, duration: 1 }, "<")
         .to(".description .t2", { opacity: 1, duration: 1 })
         .to(".description .t2", { opacity: 0, duration: 1 })
         .from(".description .t3", { opacity: 0, duration: 1 }, "<")
@@ -68,9 +72,6 @@ $(function () {
         anticipatePin: 1,
         markers: true
     });
-
-
-
     const showcase = gsap.timeline();
     showcase.to(".sc-showcase .case01 .txt", { opacity: 1, duration: 2 })
         .to(".sc-showcase .line01", { xPercent: 100 }, "text")
@@ -87,36 +88,51 @@ $(function () {
         anticipatePin: 1,
         markers: true
     });
+});
 
-    // const introTl = gsap.timeline();
-    // introTl.from(".sc-intro .description .text.t1", { opacity: 0, stagger: 0.2, y: -100, borderRadius: 200 })
-    //     .from(".sc-intro .description .text.t2", { opacity: 0, stagger: 0.2, y: -100, borderRadius: 200 })
+// const showcaseTxt = gsap.timeline();
+// showcaseTxt.to(".sc-showcase .case03 .txt", { opacity: 1, duration: 2 })
+//     .to(".sc-showcase .case03 .txt", { opacity: 0, duration: 2 })
 
-    // ScrollTrigger.create({
-    //     trigger: ".sc-intro",
-    //     start: "top top",
-    //     end: "+=600vh",
-    //     scrub: true,
-    //     pin: true,
-    //     anticipatePin: 1,
-    //     markers: true
-    // });
-})
+// ScrollTrigger.create({
+//     animation: showcaseTxt,
+//     trigger: ".case03",
+//     start: "top top",
+//     end: "+=3000",
+//     scrub: true,
+//     pin: true,
+//     markers: true
+// });
+
+// const introTl = gsap.timeline();
+// introTl.from(".sc-intro .description .text.t1", { opacity: 0, stagger: 0.2, y: -100, borderRadius: 200 })
+//     .from(".sc-intro .description .text.t2", { opacity: 0, stagger: 0.2, y: -100, borderRadius: 200 })
+
+// ScrollTrigger.create({
+//     trigger: ".sc-intro",
+//     start: "top top",
+//     end: "+=600vh",
+//     scrub: true,
+//     pin: true,
+//     anticipatePin: 1,
+//     markers: true
+// });
+
 
 
 
 
 // $(window).on('scroll', function () {
-//     var scrollTop = $(window).scrollTop();
-//     var sectionIntro = $('.sc-intro').offset().top;
-//     var sectionShowcase = $('.sc-showcase').offset().top;
-//     var sectionChallenge = $('.sc-challenge').offset().top;
-//     var sectionProve = $('.sc-prove').offset().top;
-//     var sectionGallery = $('.sc-gallery').offset().top;
-//     var sectionPossibility = $('.sc-possibility').offset().top;
-//     var sectionDataid = $('.sc-dataid').offset().top;
-//     var sectionFeature = $('.sc-feature').offset().top;
-//     var sectionGround = $('.sc-ground').offset().top;
+//     const scrollTop = $(window).scrollTop();
+//     const sectionIntro = $('.sc-intro').offset().top;
+//     const sectionShowcase = $('.sc-showcase').offset().top;
+//     const sectionChallenge = $('.sc-challenge').offset().top;
+//     const sectionProve = $('.sc-prove').offset().top;
+//     const sectionGallery = $('.sc-gallery').offset().top;
+//     const sectionPossibility = $('.sc-possibility').offset().top;
+//     const sectionDataid = $('.sc-dataid').offset().top;
+//     const sectionFeature = $('.sc-feature').offset().top;
+//     const sectionGround = $('.sc-ground').offset().top;
 
 //     $('#header').toggleClass('black', $('#heading').toggleClass('black', scrollTop >= sectionChallenge && scrollTop < sectionProve || scrollTop >= sectionProve && scrollTop < sectionGallery || scrollTop >= sectionGallery))
 // });
